8dd17aff370f29d47fd8939a9cc64159b340a687
Pablo Maldonado <xxxpabl7@gmail.com>
2026-05-13 00:26:12 -0300

    Update modelos.hpp

--- backend/include/modelos.hpp
+++ backend/include/modelos.hpp
@@ -4,22 +4,11 @@
 #include <string>
 #include <vector>
 
-
-struct Genero {
-    int id;
-    std::string nombre;
-    std::string subgenero;
-};
-
-
 struct Vinilo {
     int id;
     std::string titulo;
     std::string artista;
-    int anio;
     double precio;
-    int stock;
-    int genero_id;
 };
 
 #endif
